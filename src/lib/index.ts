41fbd329c761309f8971621b8976ac5bb2864ae5
Tim <tim@heidler.eu.org>
2023-12-25 18:42:38 +0100

    Fix data entpoint

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,9 @@
+import { PUBLIC_SITE_URL } from "$env/static/public";
+
 // place files you want to import through the `$lib` alias in this folder.
 export const getVehicleDictionary = async () => {
     try {
-        const res = await fetch("http://localhost:5173/vehicles.json");
+        const res = await fetch(PUBLIC_SITE_URL+"vehicles.json");
         const resJson= await res.json();
         return resJson;
     }catch (err) {
@@ -11,7 +13,7 @@ export const getVehicleDictionary = async () => {
 
 export const getBuildingDictionary = async () => {
     try {
-        const res = await fetch("http://localhost:5173/buildings.json");
+        const res = await fetch(PUBLIC_SITE_URL+"buildings.json");
         const resJson= await res.json();
         return resJson;
     }catch (err) {

21d06c2c530fba195e0519e6d35a6ee2fb85f51a
Tim <tim@heidler.eu.org>
2023-12-26 23:59:18 +0100

    Fix errors on /getdata

--- src/routes/getdata/+page.server.ts
+++ src/routes/getdata/+page.server.ts
@@ -3,6 +3,15 @@ import { getBuildings, getVehicles } from "$lib/lssClient";
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
+
+      //get new data
+      const vehicles = await getVehicles()
+      const buildings = await getBuildings()
+
+      if (vehicles == 1 || buildings == 1) {
+        return { data: null, error: "error: Getting data failed"}
+      }
+
     //delete old data
     const vehicles_old = await supabase.from("vehicles").select();
     let vehicles_ids = vehicles_old.data.map(vehicle => vehicle.id);
@@ -13,9 +22,7 @@ export async function load() {
     const del_old_building_data = await supabase.from("buildings").delete().in("id", buildings_ids);
 
 
-    //get new data
-    const vehicles = await getVehicles()
-    const buildings = await getBuildings()
+
 
     //insert new data
     const resVehicles = await supabase.from("vehicles").insert(vehicles);

15b68aba46836cbce56c510711d1f952b0150ca7
Tim <tim3play3@gmail.com>
2023-12-28 20:43:32 +0100

    Add checking if extension exists  (#1)
    * Add check if extension is purchased in building
    
    * Fix checking in extension exists
    
    * Update Style

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,11 +1,13 @@
 <div class="navbar">
 	<div class="flex-1">
-		<a class="btn btn-ghost text-xl normal-case text-base-content" href="/">LSS-Planner</a>
+		<a data-sveltekit-reload class="btn btn-ghost text-xl normal-case text-base-content" href="/"
+			>LSS-Planner</a
+		>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal px-1">
-			<li><a class="text-lg" href="/vehicles">Fahrzeuge</a></li>
-			<li><a class="text-lg" href="/buildings">Gebäude</a></li>
+			<li><a data-sveltekit-reload class="text-lg" href="/vehicles">Fahrzeuge</a></li>
+			<li><a data-sveltekit-reload class="text-lg" href="/buildings">Gebäude</a></li>
 		</ul>
 	</div>
 </div>
--- src/routes/buildings/+page.svelte
+++ src/routes/buildings/+page.svelte
@@ -1,7 +1,7 @@
-<script>
+<script lang="ts">
 	import { buildingDictionary, buildings } from '../stores';
 
-	function SortExtensions(building) {
+	function sortExtensions(building) {
 		let extentions = building.extensions.sort((a, b) => a.caption.localeCompare(b.caption));
 		return extentions;
 	}
@@ -11,53 +11,77 @@
 		);
 		return extentionsDictionary;
 	}
+
+	function isExtensionPurchased(building, extension) {
+		const purchasedExtensions = sortExtensions(building);
+		let purchased = false;
+		let i = 0;
+		if (purchasedExtensions.length > 0) {
+			while (i < purchasedExtensions.length) {
+				if (purchasedExtensions[i].caption == extension.caption) {
+					purchasedExtensions.shift();
+					purchased = true;
+					break;
+				}
+				i++;
+			}
+
+			return purchased;
+		}
+		return purchased;
+	}
 </script>
 
 <div class="">
 	<table>
 		<thead class="">
 			<th class="text-start text-2xl">Wachen</th>
-			<th class="flex flex-col">
-				<span>Personnel</span>
-				<span class="w-24 text-xs opacity-50">count | target</span>
+			<th>
+				<div class="flex w-24 flex-col">
+					<span>Personnel</span>
+					<span class=" text-xs opacity-50">count | target</span>
+				</div>
 			</th>
-			<th class="">extensions</th>
-			<th class="">posible extensions</th>
+			<th>
+				<div class="flex flex-col items-center">
+					<span>extensions</span>
+					<span class=" text-xs opacity-50"
+						><span class="text-success">bought</span> |
+						<span class="text-error">available</span></span
+					>
+				</div>
+			</th>
 		</thead>
 		<tbody>
 			{#each $buildings as building}
 				<tr class="">
-					<td class="border-neutral border-2 p-1">
+					<td class="border-2 border-neutral p-1">
 						<h5 class="">{building.caption}</h5>
 						<span class=" text-xs opacity-70"
 							>({#if building.small_building}
 								kleine
 							{/if}{$buildingDictionary[building.building_type].caption})</span
-						>{#if !building.enabled}<span class="bg-black px-1 text-sm text-white">6</span>{/if}
+						>{#if !building.enabled}<span class="bg-black text-white px-1 text-sm">6</span>{/if}
 					</td>
 					{#if building.personal_count > building.personal_count_target}
-						<td class="text-warning border-neutral border-2 text-center"
+						<td class="border-2 border-neutral text-center text-warning"
 							>{building.personal_count ?? 0} | {building.personal_count_target ?? 0}
 						</td>
 					{:else}
-						<td class="border-neutral border-2 text-center"
+						<td class="border-2 border-neutral text-center"
 							>{building.personal_count ?? 0} | {building.personal_count_target ?? 0}
 						</td>
 					{/if}
-					<td class="border-neutral border-2 p-1"
-						>{#each SortExtensions(building) as extension}
-							<span>{extension.caption} </span>
-							{#if !extension.enabled}
-								<span class="bg-b-fms-6 text-t-fms-6 px-1 text-sm">6</span>
+					<td class="border-2 border-neutral p-1">
+						{#each sortExtensionsDictionary(building) as extension}
+							{#if isExtensionPurchased(building, extension)}
+								<span class="text-success"> {extension.caption} <br /> </span>
+							{:else}
+								<span class="text-error"> {extension.caption} <br /> </span>
 							{/if}
-							<br />
 						{/each}
 					</td>
-					<td class="border-neutral border-2 p-1"
-						>{#each sortExtensionsDictionary(building) as extension}
-							<span>{extension.caption} <br /> </span>
-						{/each}
-					</td>
+					<td> </td>
 				</tr>
 			{/each}
 		</tbody>

4d8d606f0e501644041e03307a577045d83a4b66
Tim <tim3play3@gmail.com>
2024-01-04 00:44:58 +0100

    📈 Add sentry for error tracking

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://06079c50dc9b630a653ea6b1dca95155@o4506509773897728.ingest.sentry.io/4506509965656064',
+  tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();

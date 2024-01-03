dc47f4b01b97bd7c29aa72e33af3dd89ea03b21c
Tim <tim3play3@gmail.com>
2024-01-03 19:44:57 +0100

    ✨ Add demo user with session ID 0 for testing purposes

--- src/routes/api/[sessionID]/buildings/+server.ts
+++ src/routes/api/[sessionID]/buildings/+server.ts
@@ -1,8 +1,18 @@
 import { json } from "@sveltejs/kit";
 
+import BuildingsJson from "$lib/sampleData/buildings.json"
+
 const buildingEntpoint = "https://www.leitstellenspiel.de/api/buildings"
 
 export async function GET({ params}) {	
+    if (params.sessionID == "0"){
+        try {
+            console.log("reading demo user buildings API");
+            return json(BuildingsJson);
+        }catch (err) {
+            return  json([{"error": "error occurred trying to setup demo user"}]);
+    }
+    } else {
 
     const options = {
         method: 'GET',
@@ -18,4 +28,4 @@ export async function GET({ params}) {
         return json(resJson);
     }catch (err) {
         return  json([{"error": "invalid session id"}]);
-}}
+}}}
--- src/routes/api/[sessionID]/credits/+server.ts
+++ src/routes/api/[sessionID]/credits/+server.ts
@@ -1,21 +1,34 @@
 import { json } from "@sveltejs/kit";
 
+import CreditsJson from "$lib/sampleData/credits.json"
+
 const creditsEntpoint = "https://www.leitstellenspiel.de/api/credits"
 
-export async function GET({ params}) {	
-
-    const options = {
-        method: 'GET',
-        headers: {
-            Cookie: "_session_id="+ params.sessionID,
+export async function GET({ params, fetch}) {	
+    if (params.sessionID == "0"){
+        try {
+            console.log("reading demo user credits API");
+            return json(CreditsJson);
+        }catch (err) {
+            return  json({"error": "error occurred trying to setup demo user", "msg": err.message});
         }
-    };
+    }else {
 
-    try {
-        console.log("reading credits API");
-        const res = await fetch(creditsEntpoint, options);
-        const resJson= await res.json();
-        return json(resJson);
-    }catch (err) {
-        return  json({"error": "invalid session id"});
-}}
+
+        const options = {
+            method: 'GET',
+            headers: {
+                Cookie: "_session_id="+ params.sessionID,
+            }
+        };
+
+        try {
+            console.log("reading credits API");
+            const res = await fetch(creditsEntpoint, options);
+            const resJson= await res.json();
+            return json(resJson);
+        }catch (err) {
+            return  json({"error": "invalid session id"});
+        }
+    }
+}

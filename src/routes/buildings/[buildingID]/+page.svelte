9186f5034b19b8a57e13259bb5212ceb3fe67931
Tim <tim3play3@gmail.com>
2024-01-03 14:17:23 +0100

    ✨ Add Level cost && remaining cost table. close #5

--- src/routes/buildings/[buildingID]/+page.svelte
+++ src/routes/buildings/[buildingID]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { addSepDot, getBack, isExtensionPurchased, sortExtensionsDictionary } from '$lib';
-	import { writable } from 'svelte/store';
+	import { writable, type Writable } from 'svelte/store';
 	import { db } from '$lib/db';
 	import { liveQuery } from 'dexie';
 	import type { Building } from '@lss-manager/missionchief-type-definitions/src/api/Building.js';
@@ -15,12 +15,13 @@
 	let vehicles = liveQuery(() => db.vehicles.where('building_id').equals(id).sortBy('caption'));
 	let vehicleDictionary = liveQuery(() => db.vehicleDictionary.toArray());
 
-	let extension_cost = writable(0);
-	let vehicle_cost = writable(0);
-	let extension_remaining_cost = writable(0);
-	let level_cost = writable(0);
+	let extension_cost: Writable<number> = writable(0);
+	let vehicle_cost: Writable<number> = writable(0);
+	let extension_remaining_cost: Writable<number> = writable(0);
+	let level_cost: Writable<number> = writable(0);
+	let level_remaining_cost: Writable<number> = writable(0);
 
-	function calLevelCost(local_building: Building, local_buildingDictionary: BuildingDictionary[]) {
+	function calcLevelCost(local_building: Building, local_buildingDictionary: BuildingDictionary[]) {
 		let level = local_building.level;
 		if (level != 0) {
 			for (let i = 1; i <= level; i++) {
@@ -30,7 +31,21 @@
 		}
 	}
 
-	function calExtensionCost(
+	function calcRemainingLevelCost(
+		local_building: Building,
+		local_buildingDictionary: BuildingDictionary[]
+	) {
+		let level = local_building.level;
+		let dictLevel = local_buildingDictionary[local_building.building_type].maxLevel - 1;
+		if (level != 0 && dictLevel != 0) {
+			for (let i = level; i <= dictLevel; i++) {
+				$level_remaining_cost +=
+					local_buildingDictionary[local_building.building_type].levelPrices.credits[i];
+			}
+		}
+	}
+
+	function calcExtensionCost(
 		local_building: Building,
 		local_buildingDictionary: BuildingDictionary[]
 	) {
@@ -43,7 +58,7 @@
 		}
 	}
 
-	function calRemainingExtensionCost(
+	function calcRemainingExtensionCost(
 		local_building: Building,
 		local_buildingDictionary: BuildingDictionary[]
 	) {
@@ -56,7 +71,10 @@
 		}
 	}
 
-	function calVehicleCost(local_vehicles: Vehicle[], local_vehicleDictionary: VehicleDictionary[]) {
+	function calcVehicleCost(
+		local_vehicles: Vehicle[],
+		local_vehicleDictionary: VehicleDictionary[]
+	) {
 		if (local_vehicles.length == 0) {
 			return 0;
 		} else if (local_vehicles.length == 1) {
@@ -70,10 +88,11 @@
 	}
 
 	$: if ($building && $buildingDictionary && $vehicles && $vehicleDictionary) {
-		calRemainingExtensionCost($building, $buildingDictionary);
-		calLevelCost($building, $buildingDictionary);
-		calExtensionCost($building, $buildingDictionary);
-		calVehicleCost($vehicles, $vehicleDictionary);
+		calcLevelCost($building, $buildingDictionary);
+		calcRemainingLevelCost($building, $buildingDictionary);
+		calcExtensionCost($building, $buildingDictionary);
+		calcRemainingExtensionCost($building, $buildingDictionary);
+		calcVehicleCost($vehicles, $vehicleDictionary);
 	}
 </script>
 
@@ -82,45 +101,63 @@
 		<div class="flex items-center gap-1">
 			<button class="btn btn-ghost" on:click={getBack}>&larr;</button>
 			<h2 class="text-2xl">{$building.caption}</h2>
-			<span class="text-lg opacity-50"
-				>({#if $building.small_building}
-					kleine
-				{/if}{$buildingDictionary[$building.building_type].caption})</span
-			>
+			<span class="text-lg opacity-50">
+				({#if $building.small_building}kleine
+				{/if}{$buildingDictionary[$building.building_type].caption})
+			</span>
 		</div>
-		<table class="table table-zebra w-fit">
-			<thead><th><h3 class="text-xl">Kosten</h3></th> <th></th> <th></th></thead>
-			<tbody>
-				<tr
-					><td>Gebäude Kosten</td><td> </td><td>
-						{addSepDot($buildingDictionary[$building.building_type].credits)} ¢</td
-					>
-				</tr>
-				<tr><td>Level Kosten</td> <td> + </td><td>{addSepDot($level_cost)} ¢</td></tr>
-				<tr><td>Erweiterungs Kosten</td> <td> + </td><td>{addSepDot($extension_cost)} ¢</td></tr>
-				<tr><td>Fahrzeug Kosten</td><td> + </td><td> {addSepDot($vehicle_cost)} ¢</td></tr>
-				<tr
-					><td> Gesamt ausgegeben</td><td> = </td><td class="underline"
-						>{addSepDot(
-							$buildingDictionary[$building.building_type].credits +
-								$level_cost +
-								$extension_cost +
-								$vehicle_cost
-						)} ¢</td
-					>
-				</tr>
-				<tr
-					><td>Erweiterungs Kosten verbleibend</td><td> </td>
-					<td>{addSepDot($extension_remaining_cost)} ¢</td></tr
-				>
-			</tbody>
-		</table>
+		<div class="flex flex-wrap gap-5">
+			<table class="table table-zebra h-fit w-fit">
+				<thead><th><h3 class="text-xl">Kosten</h3></th> <th></th> <th></th></thead>
+				<tbody>
+					<tr>
+						<td>Gebäude Kosten</td><td> </td><td>
+							{addSepDot($buildingDictionary[$building.building_type].credits)} ¢
+						</td>
+					</tr>
+					<tr><td>Level Kosten</td> <td> + </td><td>{addSepDot($level_cost)} ¢</td></tr>
+					<tr><td>Erweiterungs Kosten</td> <td> + </td><td>{addSepDot($extension_cost)} ¢</td></tr>
+					<tr><td>Fahrzeug Kosten</td><td> + </td><td> {addSepDot($vehicle_cost)} ¢</td></tr>
+					<tr>
+						<td> Gesamt ausgegeben</td><td> = </td><td class="underline"
+							>{addSepDot(
+								$buildingDictionary[$building.building_type].credits +
+									$level_cost +
+									$extension_cost +
+									$vehicle_cost
+							)} ¢
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<table class="table table-zebra h-fit w-fit">
+				<thead>
+					<thead><th><h3 class="text-xl">Kosten verbleibend</h3></th> <th></th> <th></th></thead>
+				</thead>
+				<tbody>
+					<tr>
+						<td>Erweiterungs Kosten verbleibend</td><td></td>
+						<td>{addSepDot($extension_remaining_cost)} ¢</td>
+					</tr>
+					<tr>
+						<td>Level Kosten verbleibend</td><td>+</td><td
+							>{addSepDot($level_remaining_cost)} ¢
+						</td>
+					</tr>
+					<tr>
+						<td>Gesamt Kosten verbleibend</td><td>=</td><td class="underline"
+							>{addSepDot($level_remaining_cost + $extension_remaining_cost)} ¢
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 		{#if $buildingDictionary[$building.building_type].extensions.length != 0}
 			{#if sortExtensionsDictionary($building, $buildingDictionary)}
 				<div id="extensions" class="w-fit border-2 border-neutral p-2">
 					<table class="table table-zebra">
-						<thead
-							><th class="text-xl">Extensions</th>
+						<thead>
+							<th class="text-xl">Extensions</th>
 							<th class="text-center">cost</th>
 						</thead>
 						<tbody>
@@ -159,12 +196,12 @@
 						<tr class="">
 							<td class="flex items-center gap-1">
 								<h5>{vehicle.caption}</h5>
-								<span class="text-xs opacity-50"
-									>({$vehicleDictionary[vehicle.vehicle_type].caption})</span
-								>
+								<span class="text-xs opacity-50">
+									({$vehicleDictionary[vehicle.vehicle_type].caption})
+								</span>
 							</td>
-							<td class="text-center"
-								>{vehicle.max_personnel_override ?? 0} | {vehicle.assigned_personnel_count ?? 0}
+							<td class="text-center">
+								{vehicle.max_personnel_override ?? 0} | {vehicle.assigned_personnel_count ?? 0}
 							</td>
 							<td>
 								{addSepDot($vehicleDictionary[vehicle.vehicle_type].credits)} ¢
